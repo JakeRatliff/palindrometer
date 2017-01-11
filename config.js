c35038590bc4982b793e2c64a3ede385f5132e33
JakeRatliff <wrjake@gmail.com>
2017-01-11 10:16:53 -0500

    first commit

---
+++ config.js
@@ -0,0 +1,6 @@
+module.exports = {
+    consumer_key: 'i7TG5lwlNdEUyP3iRKxwVgl3j',
+    consumer_secret: 'qrUMcnvizBUIvDY2NogICJZiwCPEwLcVGAY6IqywdiDnefBipw',
+    access_token: '818539211368308736-N83vRfl9XGF0ukwfhgkpg0d4Uv5sEYJ',
+    access_token_secret: 'Tzfw9W0p9XNfKE6EXkeJLLrllRkPz5e9CvyMjc8gmsiVF'
+};
