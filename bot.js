a630dbe76093ceec535631512451d19e3f321496
JakeRatliff <wrjake@gmail.com>
2017-01-19 12:18:35 -0500

    scrubber update: remove new lines from tweets

--- bot.js
+++ bot.js
@@ -126,7 +126,7 @@ function palindrometer(x,y,z){
 
     //////////////////////////////////////////////////////////////////////////
     var combinations = [];
-    var noPunc = x.replace(/[.,\/\"#!?$%\'\^&\*;:{}=\-_`~()]/g,"");
+    var noPunc = x.replace(/[.,\/\"#/\n!?$%\'\^&\*;:{}=\-_`~()]/g,"");
     var singleSpaces = noPunc.replace(/\s{2,}/g," "); //multiple spaces to one space
     var scrubbed = singleSpaces.toLowerCase();
     var words = scrubbed.split(" ");
