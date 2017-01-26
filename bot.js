d04e77ce69e69b2b9898f10d684fa4260ecd6e07
JakeRatliff <wrjake@gmail.com>
2017-01-26 11:05:27 -0500

    reverting 2

--- bot.js
+++ bot.js
@@ -36,18 +36,13 @@ var reply = function(){
         q: '@palindrometer%20-from%3Apalindrometer', //palindrome%20OR%20palindrometer
         result_type: 'recent',
         lang: 'en'
-    };
+    }
 
     Twitter.get('search/tweets', params, function(err, data){
         if(!err){
             //console.log("data.statuses.length = " + data.statuses.length)
-            var tweets = data.statuses;
-            var tweet = tweets[0];
-            console.log(tweets.length);
-            for(i=0;i<tweets.length;i++){
-                console.log(tweets[i].user.screen_name);
-            }
-            if(tweets.length>0){
+            var tweet = data.statuses[0];
+            if(tweet){
                 MongoClient.connect(URI, function(err,db){
                     db.collection('usedTweets').findOne({"tweetId":tweet.id_str},function(err,result){
                         if(err) throw err;
@@ -55,16 +50,24 @@ var reply = function(){
                             //console.log("already found that tweet, keeping going...");
                         }else{
                             console.log("no result found, this tweet is new to me: "  + tweet.text + "\n      tweet id = " + tweet.id_str);
+                            for(i=0;i<tweet.text.length;i++){
+                                console.log(tweet.text[i]);
+                            }
                             palindrometer(tweet.text,tweet.id_str,tweet.user.screen_name);
+                            console.log("        ok, i've done something with it. now, i'm adding it to the archive...")
+                            var tweetId = tweet.id_str;
+                            db.collection('usedTweets').insertOne({"tweetId":tweetId})
                         }
                     });
                 })
             }
+
+
         }else{
             console.log("Error: " + err)
         }
     })
-};
+}
 
 //retweet();
 var twelveHours = 1000*60*60*12;
@@ -80,10 +83,10 @@ function palindrometer(x,y,z){
     var tweetId = y;
     var userName = z;
     /////////////////////////////////////////////////////////////////////////
-    function fS(x){ //or 'find symmetry'
-        var minLength = 3; //minimum character length of palindrome,
+    function fS(element){ //or 'find symmetry'
+        var minLength = 7; //minimum character length of palindrome,
         // excluding spaces and punctuation, which will have been scrubbed already.
-        //var x = element;
+        var x = element;
         if(x.length<minLength){
             return false
         }
@@ -93,11 +96,34 @@ function palindrometer(x,y,z){
             if(x[i] == x[z]){
                 console.log("match")
             }else{
-                return false
+                return "No symmetry here."
             }
         }
         console.log("Found a palindrome! It is: " + element + ". It is " + element.length + " characters long. Nice!");
-        return true
+        /*
+        Twitter.post('statuses/retweet/:id', { id: tweetId }, function (err, data, response) {
+            console.log(data)
+        })
+        */
+        var number = function(){return Math.floor(Math.random()*10)}
+        var banter = [
+            'Nice palindrome! It is',
+            'Cool - you made a palindrome that is',
+            'Good work, your palindrome is',
+            'Sweet palindrome ya got there. It is',
+            'Boo, noob! Just kidding, you made a great palindrome. It is',
+            'Ya got yerself a palindrome, there, pardner. By my reckoning, it is',
+            'Fantastic palindrome - it is',
+            'Ooh nice one. That palindrome is',
+            'Pretty cool palindrome, looks to be',
+            'Not too shabby. Your palindrome is'
+        ]
+        Twitter.post('statuses/update', { in_reply_to_status_id:tweetId, status:'@'+userName+ ' ' + banter[number()] + ' '+ element.length + ' characters long.' }, function (err, data, response) {
+            console.log(data)
+        });
+        MongoClient.connect(URI, function(err,db){
+            db.collection('usedTweets').insertOne({"tweetId":tweetId})
+        });
     }
     //////////////////////////////////////////////////////////////////////////
 
@@ -121,37 +147,7 @@ function palindrometer(x,y,z){
         return b.length - a.length;
     });
     //console.log(sortedCombos);
-    //sortedCombos.forEach(fS);
-    for(i=0;i<sortedCombos.length;i++){
-        fS(sortedCombos[i]);
-        /*
-            var number = function(){return Math.floor(Math.random()*14)};
-            var banter = [
-                'Nice palindrome! It is',
-                'Cool - you made a palindrome that is',
-                'Good work, your palindrome is',
-                'Sweet palindrome ya got there. It is',
-                'Boo, noob! Just kidding, you made a great palindrome. It is',
-                'Looks like ya got yerself a palindrome, there, pardner. By my reckoning, it is',
-                'Fantastic palindrome - it is',
-                'Ooh nice one. That palindrome is',
-                'Pretty cool palindrome, looks to be',
-                'Not too shabby. Your palindrome is',
-                'Beep. Boop. Palindrome detected. It is',
-                'I love the smell of palindromes in the morning. Especially ones that are',
-                'That\'s a spicy palindrome! It is',
-                'woah... thats, like, a dope \'drome you made bruh. its, like,'
-            ];
-            Twitter.post('statuses/update', { in_reply_to_status_id:tweetId, status:'@'+userName+ ' ' + banter[number()] + ' '+ sortedCombos[i].length + ' characters long.' }, function (err, data, response) {
-                console.log(data.text)
-            });
-            console.log("        ok, i've done something with it. now, i'm adding it to the archive...");
-            MongoClient.connect(URI, function(err,db){
-                db.collection('usedTweets').insertOne({"tweetId":tweetId})
-            });
-            */
-
-    }
+    sortedCombos.forEach(fS);
 }
 ////////////////////\\\\\\\\\\\\\\\////////////////\\\\\\\\\\\\\\///////////////\\\\\\\\\\\\\\
 
