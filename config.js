5e435db30592e2d36c3332d3628f5c00f1ca9678
JakeRatliff <wrjake@gmail.com>
2017-02-13 00:40:43 -0500

    twitter api credentials

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports = {
-    consumer_key: 'i7TG5lwlNdEUyP3iRKxwVgl3j',
-    consumer_secret: 'qrUMcnvizBUIvDY2NogICJZiwCPEwLcVGAY6IqywdiDnefBipw',
-    access_token: '818539211368308736-N83vRfl9XGF0ukwfhgkpg0d4Uv5sEYJ',
-    access_token_secret: 'Tzfw9W0p9XNfKE6EXkeJLLrllRkPz5e9CvyMjc8gmsiVF'
+    consumer_key: consumerKey,
+    consumer_secret: consumerSecret,
+    access_token: accessToken,
+    access_token_secret: accessTokenSecret
 };
